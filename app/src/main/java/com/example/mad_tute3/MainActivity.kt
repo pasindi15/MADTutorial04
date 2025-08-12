f5513202d156d23ac909bb24d725d548c0079c15
pasindi15 <pasindialawatta@gmail.com>
2025-08-12 13:44:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mad_tute3/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mad_tute3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var edtName: EditText
+    lateinit var btnWelcome: Button
+    lateinit var btnGoodBye: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
